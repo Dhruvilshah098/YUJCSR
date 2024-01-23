0cae20ec0f4e6b4342b9927a185d1c17cbe7bf0f
Dhruvilshah098 <52992665+Dhruvilshah098@users.noreply.github.com>
2024-01-24 00:57:53 +0530

    YUJCSR cso files upload

---
+++ YUJCSR.Web.CSO/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YUJCSR.Web.CSO.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Onboarding()
+        {
+            return View();
+        }
+    }
+}
