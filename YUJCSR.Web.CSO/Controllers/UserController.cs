f08a177d93a4079af67753df2591d66ee57cf65b
Fundaclear <39568335+Fundaclear@users.noreply.github.com>
2024-01-25 23:54:07 +0530

    updated

--- YUJCSR.Web.CSO/Controllers/UserController.cs
+++ YUJCSR.Web.CSO/Controllers/UserController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using YUJCSR.Web.CSO.BusinessManager;
+using YUJCSR.Web.CSO.Models;
 
 namespace YUJCSR.Web.CSO.Controllers
 {
     public class UserController : Controller
     {
+        IConfiguration _config;
+        public UserController(IConfiguration iConfig)
+        {
+            _config = iConfig;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,7 +25,22 @@ namespace YUJCSR.Web.CSO.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Register(CSOProfileModel cso)
+        {
 
+            CSOManager manager = new CSOManager(_config);
+            var status = manager.OnBoardCSO(cso);
+            if (status)
+            {
+                ViewBag.message = "Success";
+            }
+            else
+            {
+                ViewBag.message = "Failure";
+            }
+            return View();
+        }
         public IActionResult Onboarding()
         {
             return View();
