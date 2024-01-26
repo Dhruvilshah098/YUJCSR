f5685a2ff2fd24ffe196cb4fa8b607de2f2ef705
Dhruvilshah098 <52992665+Dhruvilshah098@users.noreply.github.com>
2024-01-27 01:16:56 +0530

    Project List view and fixing sign in / login forms ui

---
+++ YUJCSR.Web.CSO/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YUJCSR.Web.CSO.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
