9ca50afba00c7d551f383f7093be627d57a717f2
Dhruvilshah098 <52992665+Dhruvilshah098@users.noreply.github.com>
2024-01-28 10:47:19 +0530

    Company site code base

---
+++ YUJCSR.Web.Company/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YUJCSR.Web.Company.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ YUJCSR.Web.Company/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace YUJCSR.Web.Company.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
