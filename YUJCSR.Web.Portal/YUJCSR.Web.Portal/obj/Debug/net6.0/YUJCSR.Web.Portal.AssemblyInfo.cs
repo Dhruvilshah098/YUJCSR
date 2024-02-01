7ed3a5a51d81dfe15444b345a2bceeb6b2745970
Dhruvilshah098 <52992665+Dhruvilshah098@users.noreply.github.com>
2024-02-02 04:03:15 +0530

    about us responsive

--- YUJCSR.Web.Portal/YUJCSR.Web.Portal/obj/Debug/net6.0/YUJCSR.Web.Portal.AssemblyInfo.cs
+++ YUJCSR.Web.Portal/YUJCSR.Web.Portal/obj/Debug/net6.0/YUJCSR.Web.Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YUJCSR.Web.Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+262281933cb1a09473d79db64824d945b8309e14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("YUJCSR.Web.Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YUJCSR.Web.Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
